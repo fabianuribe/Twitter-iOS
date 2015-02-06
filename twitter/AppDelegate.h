bf40b35edcb729802e1263d8ccc2b56e89a3012e
Fabian Uribe <uribe@yahoo-inc.com>
2015-02-05 21:30:12 -0800

    Initial Commit

---
+++ twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twitter
+//
+//  Created by Fabián Uribe Herrera on 2/5/15.
+//  Copyright (c) 2015 fabian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twitter
+//
+//  Created by Fabián Uribe Herrera on 2/5/15.
+//  Copyright (c) 2015 fabian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
